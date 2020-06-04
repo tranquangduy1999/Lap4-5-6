ee244c5538d6a5b1701571d1faa14895923b1980
Administrator <Administrator@PC258>
2020-06-04 14:07:26 +0700

    Add project files.

---
+++ TranQuangDuy_lap456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranQuangDuy_lap456.MvcApplication" Language="C#" %>
---
+++ TranQuangDuy_lap456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TranQuangDuy_lap456.Startup))]
+namespace TranQuangDuy_lap456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
