a846bdb2b0e19f5a0c162392d2d770e45dc390f1
Administrator <Administrator@PC258>
2020-06-11 16:19:00 +0700

    lam bai lad

--- TranQuangDuy_lap456/Controllers/CoursesController.cs
+++ TranQuangDuy_lap456/Controllers/CoursesController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TranQuangDuy_lap456.Models;
+using TranQuangDuy_lap456.ViewModel;
 
 namespace TranQuangDuy_lap456.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+            
         }
     }
 }
